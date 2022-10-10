1e25b8e7c8a4f8a48e270695596c31d9f1950379
Anthony <anthonyfdez23@gmail.com>
2022-10-10 14:46:28 -0400

    Add zustand global store

---
+++ src/globalStore/globalStore.ts
@@ -0,0 +1,29 @@
+import create from "zustand/react";
+import { devtools, persist } from "zustand/middleware";
+import { IUser } from "./interfaces/IUser";
+
+export interface IGlobalStore {
+  user: IUser;
+  setUser: (data: IUser) => void;
+}
+
+export const useGlobalStore = create<IGlobalStore>()(
+  devtools(
+    persist((set, get) => ({
+      user: {
+        email: null,
+        email_verified: null,
+        family_name: null,
+        given_name: null,
+        name: null,
+        picture: null,
+      },
+      setUser: (data: IUser) => {
+        set({ user: data });
+      },
+    })),
+    {
+      name: "global-store",
+    }
+  )
+);
---
+++ src/globalStore/interfaces/IUser.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+  email: string | null;
+  email_verified: boolean | null;
+  family_name: string | null;
+  given_name: string | null;
+  name: string | null;
+  picture: string | null;
+}
