11c794480c788813eb43cb2b1165fb657e5103d5
Anthony <anthonyfdez23@gmail.com>
2022-10-10 14:37:24 -0400

    added auth0

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
+import React, { useEffect } from "react";
 
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsChevronDown } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import styles from "./headerDesktop.module.css";
 
-const Header: React.FC = (): JSX.Element => (
-  <header className={styles.container}>
-    <div className={styles.content}>
-      <h3>Notes App</h3>
-    </div>
-  </header>
-);
+const Header: React.FC = (): JSX.Element => {
+  const { user, isAuthenticated, loginWithRedirect } = useAuth0();
+
+  useEffect(() => {
+    console.log(user);
+    console.log(isAuthenticated);
+  }, [isAuthenticated, user]);
+
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <h3>Notes App</h3>
+        <Button onClick={() => loginWithRedirect()}>Login</Button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const darkTheme = createTheme({
@@ -13,8 +13,14 @@ const darkTheme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <Auth0Provider
+      domain="dev-bbn450zg.us.auth0.com"
+      clientId="fy3ISpMsphF9M9GuI4OnlptzVKCIxH11"
+      redirectUri={window.location.origin}
+    >
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
