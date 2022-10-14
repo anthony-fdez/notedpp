1a477786bfb2336214fb18b1c3bd8607452c94db
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-13 23:39:32 -0400

    Started making some requests

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IFolder } from "../../interfaces/IFolder";
+import Axios from "axios";
+import { useGlobalStore } from "../../globalStore/globalStore";
 
 const Dashboard = (): JSX.Element | null => {
+  const globalStore = useGlobalStore();
+
   const [isLoadingNotes, setIsLoadingNotes] = useState(false);
   const [folders, setFolders] = useState<IFolder | null>(null);
+
+  useEffect(() => {
+    setIsLoadingNotes(true);
+
+    Axios.get("http://localhost:3001/notes/get-all-folders", {
+      headers: {
+        Authorization: `Bearer ${globalStore.user.token}`,
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error: unknown) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
