d2936037cb08f3e7f2202abdbf536b24adcb4f86
Alejandro <alexvera0109@gmail.com>
2022-10-14 01:55:11 -0400

    little changes | favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { default as React, default as React } from "react";
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import BackdropSpinner from "./components/backdrop/backdrop";
