b642eb4f6294456e47b3051c613e99dad4c9d6d3
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-14 12:03:14 -0400

    created a global store mockup for the folders

--- src/globalStore/globalStore.ts
+++ src/globalStore/globalStore.ts
@@ -1,10 +1,15 @@
+import { IFolder } from "./../interfaces/IFolder";
 import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { IUser } from "./interfaces/IUser";
 
 export interface IGlobalStore {
   user: IUser;
+  folders: IFolder[] | null;
+  isLoadingFolders: boolean;
   setUser: (data: IUser) => void;
+  setFolders: (data: IFolder[]) => void;
+  setIsLoadingFolders: (isLoading: boolean) => void;
 }
 
 export const useGlobalStore = create<IGlobalStore>()(
@@ -19,8 +24,16 @@ export const useGlobalStore = create<IGlobalStore>()(
         picture: null,
         token: null,
       },
+      folders: null,
+      isLoadingFolders: false,
       setUser: (data: IUser) => {
         set({ user: data });
+      },
+      setFolders: (data: IFolder[]) => {
+        set({ folders: data });
+      },
+      setIsLoadingFolders: (isLoading: boolean) => {
+        set({ isLoadingFolders: isLoading });
       },
     })),
     {
