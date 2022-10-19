fa524e8e7a2b4d131a3d8e6ba3546f431b35a2b0
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-19 15:30:12 -0400

    Moved all requests to separate files

---
+++ src/api/notes/update/renameFolder.ts
@@ -0,0 +1,54 @@
+import { showNotification } from '@mantine/notifications';
+import Axios from 'axios';
+import { IGlobalStore } from '../../../globalStore/globalStore';
+
+interface Props {
+  globalStore: IGlobalStore;
+  folder_name: string;
+  new_folder_name: string;
+}
+
+export const renameFolder = async ({
+  globalStore,
+  folder_name,
+  new_folder_name,
+}: Props) => {
+  await Axios.patch(
+    'http://localhost:3001/notes/rename-folder',
+    {
+      folder_name,
+      new_folder_name,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${globalStore.user?.token || ''}`,
+      },
+    }
+  )
+    .then(() => {
+      showNotification({
+        title: 'Folder Renamed',
+        message: 'Your folder was renamed successfully',
+        color: 'blue',
+      });
+
+      globalStore.updateFolders();
+    })
+    .catch((e) => {
+      try {
+        if (e.response.data.message) {
+          showNotification({
+            title: 'Error',
+            message: e.response.data.message,
+            color: 'red',
+          });
+        }
+      } catch (e) {
+        showNotification({
+          title: 'Error',
+          message: 'Looks like our servers are down, try again later.',
+          color: 'red',
+        });
+      }
+    });
+};
