26e13c7e1da041b229c250963c85957b290973ac
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-24 15:27:14 -0400

    Fixed shared folder for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import BackdropSpinner from './components/backdrop/backdrop';
