e799016d1f57ab671e65f1072342e42ab4749f0f
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-27 23:53:14 -0400

    Super tiny fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
@@ -14,6 +14,7 @@ import { spotlightActions } from './spotlight/actions';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 import { routes } from './routes';
+import { useColorScheme } from '@mantine/hooks';
 
 const LightTheme = React.lazy(
   () => import('./styles/workAroundComponents/lightTheme')
@@ -27,6 +28,7 @@ function App() {
   const { isAuthenticated, logout } = useAuth0();
 
   const router = routes({ isAuthenticated });
+  const preferredColorScheme = useColorScheme();
 
   return (
     <MantineProvider
@@ -35,7 +37,7 @@ function App() {
       theme={{
         colorScheme: globalStore.theme,
         white: '#FFFFFF',
-        black: '#1a1b1e',
+        black: '#1A1B1E',
         primaryColor: 'blue',
       }}
     >
