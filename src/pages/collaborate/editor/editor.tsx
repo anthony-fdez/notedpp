61ead38e3c546520bd7e9ae160ccc9a09d190242
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-28 23:53:32 -0400

    Added some structure

--- src/pages/collaborate/editor/editor.tsx
+++ src/pages/collaborate/editor/editor.tsx
@@ -1,6 +1,24 @@
+import { Alert, Loader } from '@mantine/core';
 import React from 'react';
+import { INote } from '../../../interfaces/INote';
 
-const CollaborationEditor = () => {
+interface Props {
+  note: string | undefined;
+  noteData: INote | 'loading' | null;
+  noteId: string | null;
+  userId: string | null;
+  folderId: string | null;
+}
+
+const CollaborationEditor = ({
+  note,
+  noteId,
+  userId,
+  folderId,
+  noteData,
+}: Props) => {
+  console.log(noteData);
+
   return (
     <div>
       <h1>Collaboration Editor</h1>
