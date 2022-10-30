5d8c384095f060d3a542bf762bc0eefb09c16f3c
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-10-29 22:40:06 -0400

    Added some validation

--- src/pages/collaborate/sideMenu/modals/importNoteModal/importNoteModa.tsx
+++ src/pages/collaborate/sideMenu/modals/importNoteModal/importNoteModa.tsx
@@ -23,36 +23,63 @@ const ImportNoteModal = ({ isOpen, handleClose }: Props): JSX.Element => {
   const renderFoldersAndNotes = () => {
     if (!globalStore.folders) return null;
 
+    if (globalStore.folders.length === 0) {
+      return (
+        <>
+          <Alert title="You don't have any notes">
+            Before you are able to import notes to a collaborative session you
+            first need some notes.
+          </Alert>
+          <div className={styles.footer_container}>
+            <Button loaderPosition='left' onClick={handleClose}>
+              Got it!
+            </Button>
+          </div>
+        </>
+      );
+    }
+
     return (
-      <div className={styles.accordion_container}>
-        <Accordion>
-          {globalStore.folders.map((folder: IFolder) => {
-            return (
-              <Accordion.Item key={folder.id} value={folder.id}>
-                <Accordion.Control>{folder.folder_name}</Accordion.Control>
-                <Accordion.Panel>
-                  {folder.notes.map((note) => {
-                    return (
-                      <Button
-                        leftIcon={<AiOutlinePlus />}
-                        variant='subtle'
-                        className={styles.note_button}
-                        key={note.id}
-                        onClick={() => {
-                          setSelectedNote(note.note);
-                          setIsConfirmImportNoteModal(true);
-                        }}
-                      >
-                        {getNoteTitle({ note: note.note })}
-                      </Button>
-                    );
-                  })}
-                </Accordion.Panel>
-              </Accordion.Item>
-            );
-          })}
-        </Accordion>
-      </div>
+      <>
+        <Alert title='Override current editor'>
+          Please be aware that importing one of your notes will override all the
+          contents in the current collaboration session.
+        </Alert>
+        <div className={styles.accordion_container}>
+          <Accordion>
+            {globalStore.folders.map((folder: IFolder) => {
+              return (
+                <Accordion.Item key={folder.id} value={folder.id}>
+                  <Accordion.Control>{folder.folder_name}</Accordion.Control>
+                  <Accordion.Panel>
+                    {folder.notes.map((note) => {
+                      return (
+                        <Button
+                          leftIcon={<AiOutlinePlus />}
+                          variant='subtle'
+                          className={styles.note_button}
+                          key={note.id}
+                          onClick={() => {
+                            setSelectedNote(note.note);
+                            setIsConfirmImportNoteModal(true);
+                          }}
+                        >
+                          {getNoteTitle({ note: note.note })}
+                        </Button>
+                      );
+                    })}
+                  </Accordion.Panel>
+                </Accordion.Item>
+              );
+            })}
+          </Accordion>
+        </div>
+        <div className={styles.footer_container}>
+          <Button loaderPosition='left' type='submit'>
+            Import Note
+          </Button>
+        </div>
+      </>
     );
   };
 
@@ -64,18 +91,7 @@ const ImportNoteModal = ({ isOpen, handleClose }: Props): JSX.Element => {
         handleClose={() => setIsConfirmImportNoteModal(false)}
         handleCloseParentModal={handleClose}
       />
-      <div>
-        <Alert title='Override current editor'>
-          Please be aware that importing one of your notes will override all the
-          contents in the current collaboration session.
-        </Alert>
-        {renderFoldersAndNotes()}
-        <div className={styles.footer_container}>
-          <Button loaderPosition='left' type='submit'>
-            Import Note
-          </Button>
-        </div>
-      </div>
+      <div>{renderFoldersAndNotes()}</div>
     </Modal>
   );
 };
