9fdffa581b63aaed03e90d0e470125378577d039
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-11-03 16:58:26 -0400

    Added a landing page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,57 +1,92 @@
 import React from 'react';
-import Logo from '../../../favN.png';
-import AnimateOnScreenLoad from '../../components/animateOnScreenLoad/animateOnScreenLoad';
-import LoginButton from '../../components/auth/loginButton/loginButtonHome';
-import ThemeSwitch from '../../components/profileDrawer/settings/components/themeSwitch/themeSwitch';
-import { useGlobalStore } from '../../globalStore/globalStore';
-import notedBackground from './Back.png';
+import LoginButton from '../../components/auth/loginButton/loginButton';
 import styles from './home.module.css';
-import transparentLogo from './transparentFav.png';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Home: React.FC = (): JSX.Element => {
-  const globalStore = useGlobalStore();
-
   return (
-    <div className={styles.overall}>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.switch}>
-            <ThemeSwitch />
+    <div className={styles.container}>
+      <div className={styles.jumbo}>
+        <div className={styles.jumbo_text_container}>
+          <div>
+            <span>Noted++</span>
+            <h1>Browser based notes app for all your notes needs.</h1>
           </div>
-
-          <div className={styles.bothImages}>
-            <img src={notedBackground} alt='' className={styles.image} />
-            <img src={transparentLogo} alt='' className={styles.imageLogo} />
+          <LoginButton />
+        </div>
+        <div className={styles.bottom_fade} />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.image_section}>
+          <LazyLoadImage
+            className={styles.image}
+            alt={'First Screenshot'}
+            src='/images/1.png' // use normal <img> attributes as props
+          />
+          <div className={styles.image_right_section}>
+            <img
+              className={styles.illustration_image}
+              alt='Writer'
+              src='/writer.svg'
+            />
+            <h2 className={styles.image_section_header}>
+              Fully Feature Rich Text Editor!
+            </h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s
+            </p>
           </div>
-          <AnimateOnScreenLoad>
-            <div
-              className={`${styles.rightContainer} ${
-                globalStore.theme === 'dark'
-                  ? styles.darkContainer
-                  : styles.lightContainer
-              }`}
-            >
-              <img src={Logo} alt='' className={styles.logo} />
-              <h1
-                className={
-                  globalStore.theme === 'dark' ? styles.dark : styles.light
-                }
-              >
-                Noted++
-              </h1>
-              <h3
-                className={
-                  globalStore.theme === 'dark' ? styles.dark : styles.light
-                }
-              >
-                Become more organized.
-              </h3>
-
-              <div className={styles.button}>
-                <LoginButton />
-              </div>
-            </div>
-          </AnimateOnScreenLoad>
+        </div>
+        <div className={styles.image_section}>
+          <LazyLoadImage
+            className={styles.image}
+            alt={'First Screenshot'}
+            src='/images/2.png' // use normal <img> attributes as props
+          />
+          <div className={styles.image_right_section}>
+            <img
+              className={styles.illustration_image}
+              alt='Workflow'
+              src='/workflow.svg'
+            />
+            <h2 className={styles.image_section_header}>
+              Productivity workflow!
+            </h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+        </div>
+        <div className={styles.image_section}>
+          <LazyLoadImage
+            className={styles.image}
+            alt={'First Screenshot'}
+            src='/images/3.png' // use normal <img> attributes as props
+          />
+          <div className={styles.image_right_section}>
+            <img
+              className={styles.illustration_image}
+              alt='Collaboration'
+              src='/collaboration.svg'
+            />
+            <h2 className={styles.image_section_header}>
+              A collaboration environment.
+            </h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <img src='/icon.png' />
+          <h1>Noted++</h1>
+          <LoginButton />
         </div>
       </div>
     </div>
