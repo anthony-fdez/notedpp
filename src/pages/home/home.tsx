a77ad5be582f17b71922a312286e25334987be5c
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-11-03 22:32:23 -0400

    Redesigned the landing page jumbo

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,6 +4,7 @@ import styles from './home.module.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import { Fade } from 'react-awesome-reveal';
+import { Text } from '@mantine/core';
 
 const Home: React.FC = (): JSX.Element => {
   return (
@@ -11,17 +12,46 @@ const Home: React.FC = (): JSX.Element => {
       <div className={styles.container}>
         <div className={styles.jumbo}>
           <div className={styles.jumbo_text_container}>
-            <Fade delay={500} triggerOnce>
+            <Fade triggerOnce>
               <div>
                 <span>Noted++</span>
-                <h1>Browser based notes app for all your notes needs.</h1>
+                <h1 className={styles.header_text}>
+                  All your note needs.
+                  <Text color='blue'>All in one place.</Text>{' '}
+                </h1>
+                <LoginButton />
               </div>
-
-              <LoginButton />
             </Fade>
           </div>
+          <div className={styles.images_preview}>
+            <div className={styles.images_glow} />
+            <Parallax speed={-5}>
+              <Fade triggerOnce delay={500}>
+                <LazyLoadImage
+                  className={styles.jumbo_image_1}
+                  alt={'First Screenshot'}
+                  src='/images/1.png' // use normal <img> attributes as props
+                />
+              </Fade>
+            </Parallax>
 
-          <div className={styles.bottom_fade} />
+            <Fade triggerOnce delay={800}>
+              <LazyLoadImage
+                className={styles.jumbo_image_2}
+                alt={'First Screenshot'}
+                src='/images/2.png' // use normal <img> attributes as props
+              />
+            </Fade>
+            <Parallax speed={10}>
+              <Fade triggerOnce delay={1100}>
+                <LazyLoadImage
+                  className={styles.jumbo_image_3}
+                  alt={'First Screenshot'}
+                  src='/images/3.png' // use normal <img> attributes as props
+                />
+              </Fade>
+            </Parallax>
+          </div>
         </div>
         <div className={styles.content}>
           <Fade triggerOnce delay={500}>
