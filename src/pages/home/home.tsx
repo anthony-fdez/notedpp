aa928949bf4de4c116f560443addeaa52b1dbd41
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-11-03 22:35:47 -0400

    Moved jumbo to a different file

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,54 +5,13 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import { Fade } from 'react-awesome-reveal';
 import { Text } from '@mantine/core';
+import Jumbo from './components/jumbo/jumbo';
 
 const Home: React.FC = (): JSX.Element => {
   return (
     <ParallaxProvider>
       <div className={styles.container}>
-        <div className={styles.jumbo}>
-          <div className={styles.jumbo_text_container}>
-            <Fade triggerOnce>
-              <div>
-                <span>Noted++</span>
-                <h1 className={styles.header_text}>
-                  All your note needs.
-                  <Text color='blue'>All in one place.</Text>{' '}
-                </h1>
-                <LoginButton />
-              </div>
-            </Fade>
-          </div>
-          <div className={styles.images_preview}>
-            <div className={styles.images_glow} />
-            <Parallax speed={-5}>
-              <Fade triggerOnce delay={500}>
-                <LazyLoadImage
-                  className={styles.jumbo_image_1}
-                  alt={'First Screenshot'}
-                  src='/images/1.png' // use normal <img> attributes as props
-                />
-              </Fade>
-            </Parallax>
-
-            <Fade triggerOnce delay={800}>
-              <LazyLoadImage
-                className={styles.jumbo_image_2}
-                alt={'First Screenshot'}
-                src='/images/2.png' // use normal <img> attributes as props
-              />
-            </Fade>
-            <Parallax speed={10}>
-              <Fade triggerOnce delay={1100}>
-                <LazyLoadImage
-                  className={styles.jumbo_image_3}
-                  alt={'First Screenshot'}
-                  src='/images/3.png' // use normal <img> attributes as props
-                />
-              </Fade>
-            </Parallax>
-          </div>
-        </div>
+        <Jumbo />
         <div className={styles.content}>
           <Fade triggerOnce delay={500}>
             <div className={styles.image_section}>
